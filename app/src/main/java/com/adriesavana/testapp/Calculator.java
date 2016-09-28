0ebee64a8ff0868d97cd0f07c22ef9a9030edb1f
Adriyadi Savana <adrie4mac@gmail.com>
2016-09-28 13:45:16 +0700

    init commit

---
+++ app/src/main/java/com/adriesavana/testapp/Calculator.java
@@ -0,0 +1,24 @@
+package com.adriesavana.testapp;
+
+/**
+ * Created by adri on 9/28/16.
+ */
+
+public class Calculator {
+
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+}
